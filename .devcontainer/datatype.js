1c4efba46293a679c37349f3dafafa54b15aa62c
berendra-singh <beerendrasingh41456@gmail.com>
2024-08-30 17:25:12 +0000

    datatypes

---
+++ .devcontainer/datatype.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js code as newer version
+
+let name="anuj"
+let age=18
+let isLogged=false
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>stand alone value
+//undifined=>
+//symbol=>uniqe
+
+//object=>
---
+++ .devcontainer/varailbe.js
@@ -0,0 +1,14 @@
+const accountId=43267
+let accountMail="anujsingh798517@gmail.com"
+var accountPass="7677"
+accountCity="jaipur"
+
+accountMail="beerendrasingh41456@gmail.com"
+accountPass="798517"
+accountCity="banda"
+
+//console.table([accountId, accountMail, accountPass, accountCity])
+//console.log(accountMail)
+//console.log(accountPass)
+console.log(accountId)
+console.table([accountMail,accountPass,accountCity])
